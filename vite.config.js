019f3ca9728a0dfe3275ac90500f2b5fa29930be
Fatin Tawsif Hoque <fatintawsifhoque.fatin@gmail.com>
2025-03-09 20:04:23 +0600

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/profile-card-component/",
   plugins: [react(), tailwindcss()],
 })
