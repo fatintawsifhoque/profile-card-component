b3893fb3f2ff0455f3c7aafa6dc27024e59bdd51
Fatin Tawsif Hoque <168218628+fatintawsifhoque@users.noreply.github.com>
2025-03-09 21:45:59 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/profile-card-component/", // তোমার রিপোজিটরির নাম যোগ করো
 });
